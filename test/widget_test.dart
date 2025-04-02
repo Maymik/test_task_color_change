a2932acf28ad377cab65211198fb35d59811319c
Mike <mykhaylo.kozyrev@kallisto.dev>
2025-04-02 09:22:52 +0300

    test_change_color_on_tap

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,30 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:test_task_color_change/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Background color changes on tap', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    final Finder containerFinder = find.byType(AnimatedContainer);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    final AnimatedContainer containerWidget = tester.firstWidget(
+      containerFinder,
+    );
+    final BoxDecoration decoration =
+        containerWidget.decoration as BoxDecoration;
+    final Color initialColor = decoration.color ?? Colors.white;
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.tap(find.byType(GestureDetector));
+    await tester.pumpAndSettle();
+
+    final AnimatedContainer updatedContainerWidget = tester.firstWidget(
+      containerFinder,
+    );
+    final BoxDecoration newDecoration =
+        updatedContainerWidget.decoration as BoxDecoration;
+    final Color newColor = newDecoration.color ?? Colors.white;
+
+    expect(newColor, isNot(equals(initialColor)));
   });
 }
